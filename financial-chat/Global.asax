a1382a4f4e9dd5de656b08605d9d1faaf56a01c7
CCanas <cf.canas@gmail.com>
2022-04-15 19:14:28 -0300

    Add solution

---
+++ financial-chat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="financial_chat.MvcApplication" Language="C#" %>
---
+++ financial-chat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(financial_chat.Startup))]
+namespace financial_chat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+            ConfigureAuth(app);
+        }
+    }
+}
